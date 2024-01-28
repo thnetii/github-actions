3582460886410bf2929d23c4944fe2fe5f73e3c5
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2024-01-28 20:19:37 +0100

    Add ignore-exitcode input to gh-actions-exec action

--- run/gh-actions-exec/action.js
+++ run/gh-actions-exec/action.js
@@ -1,9 +1,32 @@
+const trueStringRegexp = /^\s*true\s*$/iu;
+const falseStringRegexp = /^\s*false\s*$/iu;
+
+/** @param {string | undefined} [value] */
+function getBooleanInput(value) {
+  let parsable = value;
+  if (typeof value === "undefined") return false;
+  if (typeof value !== "string") parsable = `${value}`;
+  parsable = value.trim();
+  if (!parsable) return false;
+  if (trueStringRegexp.test(parsable)) return true;
+  if (falseStringRegexp.test(parsable)) return false;
+  try {
+    const numbVal = parseInt(parsable, 10);
+    if (typeof numbVal === "undefined") return false;
+    if (typeof numbVal !== "number") throw new TypeError();
+    return numbVal !== 0;
+  } catch {
+    return false;
+  }
+}
+
 /**
  * @param {Pick<import("github-script").AsyncFunctionArguments, "core" | "exec">} args
  * @param {{
  *  command: string;
- *  arguments: string | undefined,
- *  "working-directory": string | undefined;
+ *  arguments?: string | undefined,
+ *  "working-directory"?: string | undefined;
+ *  "ignore-exitcode"?: string | undefined;
  * }} inputs
  */
 async function execute({ core, exec }, inputs) {
@@ -19,7 +42,7 @@ async function execute({ core, exec }, inputs) {
     ignoreReturnCode: true,
   });
   core.setOutput("command-exitcode", exitCode);
-  process.exitCode = exitCode;
+  if (!getBooleanInput(inputs["ignore-exitcode"])) process.exitCode = exitCode;
 }
 
 module.exports = execute;
